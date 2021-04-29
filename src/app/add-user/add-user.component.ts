7a80f2ce73eccc44d8d0c094300fcc3c227f6eaa
bolajahmad <56865496+bolajahmad@users.noreply.github.com>
2021-04-29 06:33:58 +0100

    initialized repository

---
+++ src/app/add-user/add-user.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.scss']
+})
+export class AddUserComponent implements OnInit {
+  public userForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    monthlyBudget: new FormControl(0, [Validators.required]),
+    phoneNumber: new FormControl('', [Validators.required])
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AddUserComponent,
+    data: {
+      title: 'Users Homepage'
+    }
+  },
+  {
+    path: 'add-user',
+    component: AddUserComponent,
+    data: {
+      title: 'Add a New User'
+    }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
